a2579ce425707b9c5ea7701dbf8ac8b3daf00b6a
Jordan H. <hobsoncjordan@gmail.com>
2024-12-22 01:02:12 -0600

    Wikipedia and Urban Dictionary commands
    Added commands for a wikipedia search and for urban dictionary searches. Also added a command to see other commands

--- bot.py
+++ bot.py
@@ -3,13 +3,106 @@ import os
 from dotenv import load_dotenv
 import random
 import requests
+import urllib.parse
+import urllib.request
 import json
 import qrcode
+import wikipediaapi
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import VerticalBarsDrawer
 from qrcode.image.styles.moduledrawers.pil import GappedSquareModuleDrawer
 
 load_dotenv()
+
+user_agent = "DiscordBot/1.0 (snoopyjchris@gmail.com) python-wikipediaapi"  # Replace with your contact info
+
+# Initialize Wikipedia API with the user agent
+wiki = wikipediaapi.Wikipedia(
+    language='en',
+    extract_format=wikipediaapi.ExtractFormat.WIKI,
+    user_agent=user_agent
+)
+
+async def urban_search(message):
+    query = message.content[7:]
+    if not query:
+        await message.channel.send("Please provide a term to look up! Use: `$urban <term>`")
+        return
+
+    try:
+        # Get the data
+        term = urllib.parse.quote(query)
+        url = f"https://api.urbandictionary.com/v0/define?term={term}"
+        headers = {
+            'User-Agent': 'DiscordBot/1.0'
+        }
+        
+        req = urllib.request.Request(url, headers=headers)
+        response = urllib.request.urlopen(req)
+        data = json.loads(response.read().decode())
+        
+        if not data['list']:
+            await message.channel.send("No definitions found for that term!")
+            return
+            
+        entry = data['list'][0]
+        
+        # Create embed
+        embed = discord.Embed(
+            title=entry['word'],
+            url=entry['permalink'],
+            color=0x00ff00
+        )
+        
+        # Clean up definition and example
+        definition = entry['definition'].replace('[', '').replace(']', '')
+        example = entry['example'].replace('[', '').replace(']', '')
+        
+        # Add fields
+        embed.add_field(
+          name="Definition", 
+            value=definition[:1024] if len(definition) > 1024 else definition, 
+            inline=False
+        )
+        
+        if example:
+            embed.add_field(
+                name="Example", 
+                value=example[:1024] if len(example) > 1024 else example, 
+                inline=False
+            )
+        
+        embed.add_field(
+            name="Rating", 
+            value=f"👍 {entry['thumbs_up']} | 👎 {entry['thumbs_down']}", 
+            inline=True
+        )
+        
+        embed.set_footer(text=f"Definition by {entry['author']}")
+        
+        await message.channel.send(embed=embed)
+        
+    except Exception as e:
+        await message.channel.send(f"An error occurred: {str(e)}")
+
+# Add these functions somewhere in your code
+def get_wiki(query, sentences=3):
+    """Get a Wikipedia summary for the given query"""
+    try:
+        page = wiki.page(query)
+        
+        if not page.exists():
+            return "Sorry, I couldn't find any Wikipedia article matching that query."
+        
+        # Get the first few sentences of the summary
+        summary = page.summary[0:1000]  # Limit to 1000 characters
+        if len(summary) == 1000:
+            summary = summary[:summary.rindex('.')] + '...'
+        
+        return f"**{page.title}**\n{summary}\n\nRead more: {page.fullurl}"
+    
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 def get_cat():
    response = requests.get('https://api.thecatapi.com/v1/images/search?api_key={}'.format(os.getenv('CAT_KEY')))
@@ -106,62 +199,65 @@ class MyClient(discord.Client):
   async def on_message(self,message):
     if message.author == self.user:
       return
-    elif message.content.startswith('$hello'):
-      await message.channel.send('Hello Darling!')
-    elif message.content.startswith('$pat'):
-      await message.channel.send('**Wags tail enthusiastically** Arf arf!')
-    elif message.content.startswith('$roll'):
-      luckyRoll = random.randint(1,20)
-      match luckyRoll:
-        case 1:
-          retString = "Your roll is... A natural {}. Oof...".format(luckyRoll)
-          print(retString)
-        case 20:
-          retString = "Your roll is... A NATURAL {}! Awesome!".format(luckyRoll)
-          print(retString)
-        case _:
-          retString = "Your roll is... [{}]".format(luckyRoll)
-          print(retString)
-      await message.channel.send(retString)
-      # if luckyRoll == 20:
-      #   retString = "Your roll is... A NATURAL {}! Awesome!".format(luckyRoll)
-      #   print(retString)
-      # elif luckyRoll == 1:
-      #   retString = "Your roll is... A NATURAL {}! Less awesome...".format(luckyRoll)
-      #   print(retString)
-      # else:
-      #   retString = "Your roll is... [{}]".format(luckyRoll)
-      #   print(retString)
-      
-    elif message.content.startswith('$meow'):
+    
+    if not message.content.startswith('$'):
+      return
+
+    command = message.content[1:].split()[0].lower()  # Get the command part after '$'
+    
+    match command:
+    
+        case 'hello':
+            await message.channel.send('Hello Darling!')
+        case 'pat':
+            await message.channel.send('**Wags tail enthusiastically** Arf arf!')
+        case 'roll':
+            luckyRoll = random.randint(1,20)
+            match luckyRoll:
+                case 1:
+                    retString = "Your roll is... A natural {}. Oof...".format(luckyRoll)
+                case 20:
+                    retString = "Your roll is... A NATURAL {}! Awesome!".format(luckyRoll)
+                case _:
+                    retString = "Your roll is... [{}]".format(luckyRoll)
+            print(retString)
+            await message.channel.send(retString)
+        case 'meow':
             await message.channel.send(get_cat())
-    elif message.content.startswith('$bark'):
+        case 'bark':
             await message.channel.send(get_dog())
-    elif message.content.startswith('$qr'):
-       print(message.content[4:]) #Link for QR Code
-       link = message.content[4:]
-       get_qr(link)
-       print('Pow')
-       await message.channel.send(f'Here you go, {message.author}!')
-       await message.channel.send(file=discord.File('qrImage.png'))
-       print('Job done :P')
-    elif message.content.startswith('$fact'):
-       await message.channel.send(get_educated())
-    elif message.content.startswith('$pun'):
-       await message.channel.send(get_punny())
-    elif message.content.startswith('$weather'):
-       city = message.content[9:]
-       print(city)
-       await message.channel.send(get_webby(city))
-    elif message.content.startswith('$ver'):
-       link = message.content[5:]
-       await message.channel.send(get_verified(link))
-       print('Verfication Done')
-    elif message.content.startswith('$phone'):
-      number = message.content[7:]
-      print(number)
-      await message.channel.send(get_phoneNum(number))
-      print('Number done')
+        case 'qr':
+            link = message.content[4:]
+            print(f'Link for QR Code: {link}')
+            get_qr(link)
+            await message.channel.send(f'Here you go, {message.author.mention}!')
+            await message.channel.send(file=discord.File('qrImage.png'))
+            print('Job done :P')
+        case 'fact':
+            await message.channel.send(get_educated())
+        case 'pun':
+            await message.channel.send(get_punny())
+        case 'weather':
+            city = message.content[9:]
+            print(city)
+            await message.channel.send(get_webby(city))
+        case 'ver':
+            link = message.content[5:]
+            await message.channel.send(get_verified(link))
+            print('Verfication Done')
+        case 'phone':
+            number = message.content[7:]
+            print(number)
+            await message.channel.send(get_phoneNum(number))
+            print('Number done')
+        case 'help':
+            await message.channel.send('Here are the commands you can use: \n $hello - Say hello to the bot \n $pat - Pat the bot on the head \n $roll - Roll a 20 sided die \n $meow - Get a random cat picture \n $bark - Get a random dog picture \n $qr - Generate a QR code for a given link \n $fact - Get a random fact \n $pun - Get a random pun \n $weather - Get the weather for a given city \n $ver - Verify a given URL \n $phone - Verify a given phone number \n $wiki - Get a summary of a Wikipedia article \n $urban - Search for a term on Urban Dictionary')
+        case 'wiki':
+            query = message.content[6:]
+            await message.channel.send(get_wiki(query))
+        case 'urban':
+            await urban_search(message)
+    
 
 intents = discord.Intents.default()
 intents.message_content = True
